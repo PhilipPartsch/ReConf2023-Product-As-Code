3206518ed2f2e5218537c6e0a91b9f64fd6cfd42
PhilipPartsch <95444300+PhilipPartsch@users.noreply.github.com>
2024-04-17 16:18:32 +0200

    Update metamodel.py

--- docs/metamodel.py
+++ docs/metamodel.py
@@ -319,7 +319,7 @@ needs_warnings = {
     'stakeholder_requirement_without_author': "type == 'stake_req' and not author",
     'author_only_allowed_for_stakeholder_requirement': "type != 'stake_req' and author != ''",
     'evaluation_output_and_evaluated_needed': "type == 'evaluation' and (len(output) == 0 or len(evaluated) == 0)",
-    'invalid_status' : "status is not None and status not in ['accepted', 'implemented', 'verified']",
+    'invalid_status' : "status is not None and status not in ['new', 'changed', 'accepted', 'implemented', 'verified']",
 }
 
 
